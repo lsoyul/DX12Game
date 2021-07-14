57bd9f3cda326321c414a640ceafc72acc95a510
sgykim15 <sgykim36@gmail.com>
2021-07-15 00:19:31 +0900

    - Input, Timer 기본기능 추가

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -8,14 +8,6 @@ void Engine::Init(const WindowInfo& info)
 	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
 	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
 
-	_device = make_shared<Device>();
-	_cmdQueue = make_shared<CommandQueue>();
-	_swapChain = make_shared<SwapChain>();
-	_rootSignature = make_shared<RootSignature>();
-	_cb = make_shared<ConstantBuffer>();
-	_tableDescHeap = make_shared<TableDescriptorHeap>();
-	_depthStencilBuffer = make_shared<DepthStencilBuffer>();
-
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
 	_swapChain->Init(info, _device->GetDevice(), _device->GetDXGI(), _cmdQueue->GetComQueue());
@@ -23,6 +15,9 @@ void Engine::Init(const WindowInfo& info)
 	_cb->Init(sizeof(Transform), 256);
 	_tableDescHeap->Init(256);
 	_depthStencilBuffer->Init(_window);
+
+	_input->Init(info.hwnd);
+	_timer->Init();
 
 	ResizeWindow(info.width, info.height);
 }
@@ -34,6 +29,14 @@ void Engine::Render()
 	// TODO: 물체 그려주기
 
 	RenderEnd();
+}
+
+void Engine::Update()
+{
+	_input->Update();
+	_timer->Update();
+
+	ShowFps();
 }
 
 void Engine::RenderBegin()
@@ -57,4 +60,14 @@ void Engine::ResizeWindow(int32 width, int32 height)
 	// :: => Global namespace 에서 함수를 찾겠다. => Windows 관련 기능
 
 	_depthStencilBuffer->Init(_window);
+}
+
+void Engine::ShowFps()
+{
+	uint32 fps = _timer->GetFps();
+
+	WCHAR text[100] = L"";
+	::wsprintf(text, L"FPS : %d", fps);
+
+	::SetWindowText(_window.hwnd, text);
 }
